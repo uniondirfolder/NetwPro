ec2a27f806de90640484c962de619b0591902a22
HanNabu <uniondirfolder@gmail.com>
2020-10-02 23:25:09 +0300

    Core not tr

---
+++ gl_001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace gl_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("207.46.197.32");
+            IPEndPoint ep = new IPEndPoint(ip, 80);
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+        }
+    }
+}
