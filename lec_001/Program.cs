d1a4e6be7dfeab705aac7b09e0a72873497b73a6
HanNabu <uniondirfolder@gmail.com>
2020-10-02 22:52:04 +0300

    end

--- lec_001/Program.cs
+++ lec_001/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
 
 namespace lec_001
 {
@@ -6,7 +8,30 @@ namespace lec_001
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ping");
+            using (Ping ping = new Ping())
+            {
+                try
+                {
+                    PingReply pingReply = ping.Send("127.0.0.1", 100);
+                    if (pingReply.Status==IPStatus.Success)
+                    {
+                        Console.WriteLine($"{pingReply.Address} {pingReply.RoundtripTime}");
+                    }
+                    else
+                    {
+                        Console.WriteLine(pingReply.Status);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
