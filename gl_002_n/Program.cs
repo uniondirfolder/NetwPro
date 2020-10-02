2b3000ad0369b2351f7513278a571c17e77cece1
HanNabu <uniondirfolder@gmail.com>
2020-10-03 00:21:39 +0300

    2+

---
+++ gl_002_n/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gl_002_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip,25);
+
+            socket.Bind(ep);
+            socket.Listen(10);
+
+            try
+            {
+                while (true)
+                {
+                    Socket ns = socket.Accept();
+                    Console.WriteLine(ns.RemoteEndPoint.ToString());
+                    ns.Send(Encoding.ASCII.GetBytes(DateTime.Now.ToString()));
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
