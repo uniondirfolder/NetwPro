5e0e9274ddf01b709e4c51c74fc15fed5bba8287
HanNabu <uniondirfolder@gmail.com>
2020-10-03 03:17:29 +0300

    5+

---
+++ gl_005_test_udp_async/MainF.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace gl_005_test_udp_async
+{
+    partial class MainF
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_send = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.tb_main = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_send
+            // 
+            this.tb_send.Location = new System.Drawing.Point(12, 228);
+            this.tb_send.Name = "tb_send";
+            this.tb_send.Size = new System.Drawing.Size(348, 20);
+            this.tb_send.TabIndex = 17;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(366, 228);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 16;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(366, 12);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 15;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(93, 12);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 14;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // tb_main
+            // 
+            this.tb_main.Location = new System.Drawing.Point(12, 52);
+            this.tb_main.Multiline = true;
+            this.tb_main.Name = "tb_main";
+            this.tb_main.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_main.Size = new System.Drawing.Size(429, 168);
+            this.tb_main.TabIndex = 13;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(12, 12);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 12;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 272);
+            this.Controls.Add(this.tb_send);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.tb_main);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_send;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.TextBox tb_main;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ gl_005_test_udp_async/MainF.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gl_005_test_udp_async
+{
+    public partial class MainF : Form
+    {
+        delegate void AddTextDelegate(string text);
+        private class StateObject 
+        {
+            public Socket workSocket = null;
+            public const int BufferSize = 1024;
+            public byte[] buffer = new byte[BufferSize];
+        }
+        StateObject state = new StateObject();
+        Socket socket;
+        IAsyncResult RcptRes, SendRes;
+
+        EndPoint ClientEP = new IPEndPoint(IPAddress.Any, 100);
+        public MainF()
+        {
+            InitializeComponent();
+        }
+
+        void AddText(string text) 
+        {
+            tb_main.Text += text;
+        }
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (socket != null) return;
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 100));
+
+            state.workSocket = socket;
+            RcptRes = socket.BeginReceiveFrom(
+                state.buffer,
+                0,
+                StateObject.BufferSize,
+                SocketFlags.None,
+                ref ClientEP,
+                new AsyncCallback(Receive_Completed), state);
+        }
+
+        private void Receive_Completed(IAsyncResult ar)
+        {
+            try
+            {
+                StateObject so = (StateObject)ar.AsyncState;
+                Socket client = so.workSocket;
+                if (socket == null) return;
+                int readed = client.EndReceiveFrom(RcptRes, ref ClientEP);
+                string strClientIP = ((IPEndPoint)ClientEP).Address.ToString();
+                string str = string.Format($"\nReceived from {strClientIP}\r\n{Encoding.Unicode.GetString(so.buffer, 0, readed)}");
+
+                tb_main.BeginInvoke(new AddTextDelegate(AddText), str);
+
+                RcptRes = socket.BeginReceiveFrom(
+                    state.buffer,
+                    0,
+                    StateObject.BufferSize,
+                    SocketFlags.None,
+                    ref ClientEP,
+                    new AsyncCallback(Receive_Completed),
+                    state);
+            }
+            catch (SocketException ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            socket.Shutdown(SocketShutdown.Receive);
+            socket.Close();
+            socket = null;
+            tb_main.Text = "";
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            tb_main.Text = "";
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            byte[] buffer = Encoding.Unicode.GetBytes(tb_send.Text);
+            SendRes = socket.BeginSendTo(
+                buffer,
+                0, 
+                buffer.Count(),
+                SocketFlags.None,
+                (EndPoint)new IPEndPoint(IPAddress.Parse("127.0.0.1"), 100),
+                new AsyncCallback(Send_Completed),
+                socket);
+        }
+
+        private void Send_Completed(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            socket.EndSend(SendRes);
+            socket.Shutdown(SocketShutdown.Send);
+            socket.Close();
+        }
+    }
+}
