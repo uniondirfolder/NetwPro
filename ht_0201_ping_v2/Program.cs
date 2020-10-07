106cd45d67d93b1af9895ec349a373bfcd7ecfb7
HanNabu <uniondirfolder@gmail.com>
2020-10-07 17:54:31 +0300

    leo2 +

---
+++ ht_0201_ping_v2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace ht_0201_ping_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "0201 ping 192.168.43.0/24";
+            int count = 102;
+            string ipBase = "192.168.115.";
+            if (!File.Exists(Directory.GetCurrentDirectory() + @"\ping.txt")) { File.CreateText(Directory.GetCurrentDirectory() + @"\ping.txt"); }
+            else { File.WriteAllText(Directory.GetCurrentDirectory() + @"\ping.txt", ""); }
+            while (count!=100)
+            {
+                count--;
+                string ip = ipBase + count.ToString();
+                Ping ping = new Ping();
+                PingReply reply = ping.Send(ip);
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine(ip+" is active");
+                    File.WriteAllText(Directory.GetCurrentDirectory() + @"\ping.txt",$"{ip} is active\r\n");
+                    
+                }
+                else
+                {
+                    Console.WriteLine(ip + " no information");
+                    File.WriteAllText(Directory.GetCurrentDirectory() + @"\ping.txt", $"{ip}  no information\r\n");
+                    
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
