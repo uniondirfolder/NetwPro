b0c4496f2803ca4680322cfc48363476c706a44b
HanNabu <uniondirfolder@gmail.com>
2020-10-14 17:34:45 +0300

    add new

---
+++ dn_003_UdpClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dn_003_UdpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 10_000;
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, port);
+
+            socket.Bind(ep);
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            byte[]buff = new byte[255];
+
+            while (true)
+            {
+                int count = socket.ReceiveFrom(buff, ref remoteEP);
+                string msg = Encoding.UTF8.GetString(buff, 0, count);
+                Console.WriteLine($"Received from {remoteEP} msg: {msg}");
+            }
+        }
+    }
+}
---
+++ dn_003_UdpServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dn_003_UdpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 10_000;
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"),port );
+
+            while (true)
+            {
+                string time = DateTime.Now.ToLongTimeString();
+                Console.WriteLine($"Sended: {time}");
+                byte[] msg = Encoding.UTF8.GetBytes(time);
+                socket.SendTo(msg, ep);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
