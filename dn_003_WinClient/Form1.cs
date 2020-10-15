6028c1d27c1d10cbc39ab6cc241588676c1ab749
HanNabu <uniondirfolder@gmail.com>
2020-10-15 14:03:58 +0300

    888

---
+++ dn_003_WinClient/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dn_003_WinClient
+{
+    public partial class Form1 : Form
+    {
+        UdpClient client = new UdpClient();
+        private const string msg = "{0} : {1}";
+
+        public Form1()
+        {
+            InitializeComponent();
+            Listener();
+        }
+
+        private void Listener()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.SendBroadcast(String.Format(msg,"max,", textBox1.Text));
+        }
+    }
+}
---
+++ dn_003_WinClient/UdpClientExtention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dn_003_WinClient
+{
+    public static class UdpClinettExtention
+    {
+        public static void SendBroadcast(this UdpClient udpClient, string msg)
+        {
+            var data = Encoding.Unicode.GetBytes(msg);
+            udpClient.Send(data, data.Length, "192.168.115.255", 10_000);
+        }
+    }
+}
